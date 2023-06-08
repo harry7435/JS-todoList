ef91c8cca46813be2038371977f1fd5352b2dd09
Cho-Ik-Jun <harry960629@naver.com>
2023-06-09 01:06:56 +0900

    Feat: 3x3 드럼 동작 로직 구현

--- 3x3 durm/script.js
+++ 3x3 durm/script.js
@@ -1,15 +1,19 @@
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
 
   const get = function (target) {
-    return document.querySelector(target)
-  }
+    return document.querySelector(target);
+  };
 
   const getAll = function (target) {
-    return document.querySelectorAll(target)
-  }
+    return document.querySelectorAll(target);
+  };
 
-  const soundsRoot = 'assets/sounds/'
+  const keys = Array.from(getAll('.key'));
+
+  console.log(keys);
+
+  const soundsRoot = 'assets/sounds/';
   const drumSounds = [
     { key: 81, sound: 'clap.wav' },
     { key: 87, sound: 'crash.wav' },
@@ -20,5 +24,51 @@
     { key: 90, sound: 'shaker.wav' },
     { key: 88, sound: 'snare.wav' },
     { key: 67, sound: 'tom.wav' },
-  ]
-})()
+  ];
+
+  const getAudioElement = (index) => {
+    const audio = document.createElement('audio');
+    audio.dataset.key = drumSounds[index].key;
+    audio.src = soundsRoot + drumSounds[index].sound;
+    return audio;
+  };
+
+  const playSound = (keycode) => {
+    const $audio = get(`audio[data-key="${keycode}"]`);
+    const $key = get(`div[data-key="${keycode}"]`);
+
+    if ($audio && $key) {
+      $key.classList.add('playing');
+      $audio.currentTime = 0;
+      $audio.play();
+    }
+  };
+
+  const onKeyDown = (e) => {
+    playSound(e.keyCode);
+  };
+
+  const onMouseDown = (e) => {
+    const keycode = e.target.getAttribute('data-key');
+    playSound(keycode);
+  };
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName === 'transform') {
+      e.target.classList.remove('playing');
+    }
+  };
+
+  const init = () => {
+    window.addEventListener('keydown', onKeyDown);
+    keys.forEach((key, index) => {
+      const audio = getAudioElement(index);
+      key.appendChild(audio);
+      key.dataset.key = drumSounds[index].key;
+      key.addEventListener('click', onMouseDown);
+      key.addEventListener('transitionend', onTransitionEnd);
+    });
+  };
+
+  init();
+})();
