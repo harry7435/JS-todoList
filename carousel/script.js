7fad6f9da7869ddddd0571b21a7c52cfa5bf17e7
Cho-Ik-Jun <harry960629@naver.com>
2023-06-13 23:17:31 +0900

    Feat: 슬라이드 이동 중 클릭 이벤트 막기 구현

--- carousel/script.js
+++ carousel/script.js
@@ -13,12 +13,21 @@
 
       this.totalItems = this.items.length;
       this.current = 0;
+      this.isMoving = false;
     }
 
     initCarousel() {
+      this.isMoving = false;
       this.items[0].classList.add('active');
       this.items[1].classList.add('next');
       this.items[this.totalItems - 1].classList.add('prev');
+    }
+
+    disabledInteraction() {
+      this.isMoving = true;
+      setTimeout(() => {
+        this.isMoving = false;
+      }, 500);
     }
 
     setEventListener() {
@@ -39,6 +48,8 @@
     }
 
     moveCarouselTo() {
+      if (this.isMoving) return;
+      this.disabledInteraction();
       let prev = this.current - 1;
       let next = this.current + 1;
 
@@ -62,6 +73,7 @@
     }
 
     moveNext() {
+      if (this.isMoving) return;
       if (this.current === this.totalItems - 1) {
         this.current = 0;
       } else {
@@ -71,6 +83,7 @@
     }
 
     movePrev() {
+      if (this.isMoving) return;
       if (this.current === 0) {
         this.current = this.totalItems - 1;
       } else {
