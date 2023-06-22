153f2f5228409f07b9b314e1a56d8e1d33e372a6
Cho-Ik-Jun <harry960629@naver.com>
2023-06-22 20:42:56 +0900

    Feat: 모달창 오픈 구현

--- modalPopup/script.js
+++ modalPopup/script.js
@@ -1,6 +1,19 @@
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
   const get = (target) => {
-    return document.querySelector(target)
-  }
-})()
+    return document.querySelector(target);
+  };
+
+  const $button = get('.modal_open_button');
+  const $modal = get('.modal');
+  const $body = get('body');
+
+  const toggleModal = () => {
+    $modal.classList.toggle('show');
+    $body.classList.toggle('scroll_lock');
+  };
+
+  $button.addEventListener('click', () => {
+    toggleModal();
+  });
+})();
