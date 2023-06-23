5c791dbb38cf985ceb5ebac3f6bd3b776ff38029
Cho-Ik-Jun <harry960629@naver.com>
2023-06-23 09:25:08 +0900

    Feat: 3D 캐러셀 회전 기능 구현

--- 3d carousel/script.js
+++ 3d carousel/script.js
@@ -1,7 +1,29 @@
-;(function () {
-  'use strict'
+(function () {
+  'use strict';
 
   const get = (target) => {
-    return document.querySelector(target)
-  }
-})()
+    return document.querySelector(target);
+  };
+
+  const cellCount = 6;
+  let selectIndex = 0;
+  const carousel = get('.carousel');
+  const prevButton = get('.prev_button');
+  const nextButton = get('.next_button');
+
+  const rotateCarousel = () => {
+    // 실제로는 반대로 각도를 돌려야 정방향으로 회전함
+    const angle = (selectIndex / cellCount) * -360;
+    carousel.style.transform = `translateZ(-346px) rotateY(${angle}deg)`;
+  };
+
+  prevButton.addEventListener('click', () => {
+    selectIndex--;
+    rotateCarousel();
+  });
+
+  nextButton.addEventListener('click', () => {
+    selectIndex++;
+    rotateCarousel();
+  });
+})();
