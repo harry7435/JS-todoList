2b7d506b3ec52d4c79b25c8bd0425a16f1fd99c9
Cho-Ik-Jun <harry960629@naver.com>
2023-06-27 10:02:02 +0900

    Feat: 벽돌게임 캔버스 생성

--- breakout/script.js
+++ breakout/script.js
@@ -1,3 +1,99 @@
-;(function () {
-  'use strict'
-})()
+(function () {
+  'use strict';
+
+  const get = (element) => document.querySelector(element);
+
+  class BrickBreak {
+    constructor(parent = 'body', data = {}) {
+      this.parent = get(parent);
+      this.canvas = document.createElement('canvas');
+      this.canvas.setAttribute('width', 480);
+      this.canvas.setAttribute('height', 340);
+      this.ctx = this.canvas.getContext('2d');
+      this.fontFamily =
+        "20px -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+      this.score = 0;
+      this.lives = data.lives;
+      this.speed = data.speed;
+      this.image = document.createElement('img');
+      this.bg = data.bg;
+      this.radius = 10;
+      this.ballX = this.canvas.width / 2;
+      this.ballY = this.canvas.hegith - 30;
+      this.directX = data.speed;
+      this.directY = -data.speed;
+      this.paddleWidth = data.paddleWidth;
+      this.paddleHeight = data.paddleHeight;
+      this.rightPressed = false;
+      this.leftPressed = false;
+      this.paddleX = (this.canvas.width - this.paddleWidth) / 2;
+      this.brickCol = data.brickCol;
+      this.brickWidth = data.brickWidth;
+      this.brickHeight = data.brickHeight;
+      this.brickPad = data.brickPad;
+      this.brickPosX = data.brickPosX;
+      this.brickPosY = data.brickPosY;
+      this.ballColor = data.paddleColor;
+      this.fontColor = data.fontColor;
+      this.brickStartColor = data.brickStartColor;
+      this.brickEndColor = data.brikEndColor;
+      this.image.setAttribute('src', this.bg);
+      this.parent.appendchild(this.canvas);
+      this.bricks = [];
+    }
+
+    init = () => {
+      // 벽돌 배열 만들기
+      for (let colIndex = 0; colIndex < this.brickCol.length; colIndex++) {
+        this.bricks[colIndex] = [];
+        for (let rowIndex = 0; rowIndex < this.brickRow.length; rowIndex++) {
+          this.bricks[colIndex][rowIndex] = { x: 0, y: 0, status: 1 };
+        }
+      }
+
+      // this.keyEvent()
+      this.draw();
+    };
+
+    draw = () => {
+      // 캔버스 초기화
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.ctx.drawImage(
+        this.image,
+        this.canvas.width / 2 - this.image.width / 2,
+        this.canvas.height / 2 - this.image.height / 2
+      );
+
+      this.drawBall();
+      this.drawPaddle();
+      this.drawBricks();
+      this.drawScore();
+      this.drawLives();
+      this.detectCollision();
+    };
+  }
+
+  const data = {
+    lives: 5,
+    speed: 2,
+    paddleHeight: 10,
+    paddleWidth: 75,
+    bg: './assets/bg.jpeg',
+    ballColor: '#04bf55',
+    paddleColor: '#05aff2',
+    fontColor: '#f2bb16',
+    brickStartColor: '#f29f05',
+    brikEndColor: '#f21905',
+    brickRow: 3,
+    brinkCol: 5,
+    brickWidth: 75,
+    brickHeight: 70,
+    brickPad: 10,
+    brickPosX: 30,
+    brickPosY: 30,
+  };
+
+  const brickBreak = new BrickBreak('.canvas', data);
+  brickBreak.init();
+})();
